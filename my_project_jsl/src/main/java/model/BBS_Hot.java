9b7c8a92466f0a20981be07f78c6a120f00f88c5
user <user@DESKTOP-M8J6CQV>
2020-06-23 17:37:35 +0900

    aa

---
+++ my_project_jsl/src/main/java/model/BBS_Hot.java
@@ -0,0 +1,20 @@
+package model;
+
+public class BBS_Hot { //추천검사용모델
+	
+ private String formal_user_id;
+ private Integer bbs_seqno;
+public String getFormal_user_id() {
+	return formal_user_id;
+}
+public void setFormal_user_id(String formal_user_id) {
+	this.formal_user_id = formal_user_id;
+}
+public Integer getBbs_seqno() {
+	return bbs_seqno;
+}
+public void setBbs_seqno(Integer bbs_seqno) {
+	this.bbs_seqno = bbs_seqno;
+}
+ 
+}
